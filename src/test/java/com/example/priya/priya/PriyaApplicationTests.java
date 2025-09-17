0095c6bb6955ae9954adc73e06c7c21f5f6afb33
Priyanka-B18 <727723euai093@skcet.ac.in>
2025-09-17 09:21:40 +0530

    first commit

---
+++ src/test/java/com/example/priya/priya/PriyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.priya.priya;
+
+import org.junit.j                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             upiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
