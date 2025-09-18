437fd319ad70aa98db41261ef767ce34720ff43e
Priyanka-B18 <727723euai093@skcet.ac.in>
2025-09-18 15:05:52 +0530

    updated application

--- src/main/java/com/example/priya/priya/PriyaApplication.java
+++ src/main/java/com/example/priya/priya/PriyaApplication.java
@@ -8,7 +8,7 @@ public class PriyaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PriyaApplication.class, args);
-	    System.out.println("Hello World");
+	    System.out.println("Appdevelopment");
 	}
 
 }
